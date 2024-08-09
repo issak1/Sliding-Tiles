4b1534d4d1fba4439061ea6f556688708e5675b1
issak1 <143627558+issak1@users.noreply.github.com>
2024-08-09 19:58:24 -0400

    Update Tile.cpp

--- Tile.cpp
+++ Tile.cpp
@@ -1,1 +1,18 @@
 #include "Tile.h"
+
+Tile::Tile(int value) : value(value)) {}
+
+int Tile::getValue() const 
+{
+  return value;
+}
+
+void Title::setValue(int value)
+{
+  this->value = value;
+}
+
+bool Title::isEmpty() const 
+{
+  return value == 0;
+}
