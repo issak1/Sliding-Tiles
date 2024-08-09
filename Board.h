daec3897b447af970ee8d92a815ed7e319040b26
issak1 <143627558+issak1@users.noreply.github.com>
2024-08-09 19:59:30 -0400

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,2 +1,12 @@
 #include "Tile.h"
 #include <vector>
+
+class Board {
+public:
+    Board(int size);
+
+    void shuffle();
+    bool moveTile(int tileValue);
+    bool isSolved() const;
+    void printBoard() const;
+}
