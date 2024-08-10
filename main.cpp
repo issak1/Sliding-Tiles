2ee2b29567c8d7e2b8749f13fd339ae0a608c98e
issak1 <143627558+issak1@users.noreply.github.com>
2024-08-09 20:15:02 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,23 @@
 
 int main()
 {
-  int size;
+  int size = 3;
+  Board board(size);
+  
+  board.shuffle();
 
+  while(!board.isSolved())
+    {
+      board.printBoard();
+
+      int move;
+      std::cout << "Enter tile number to move: ";
+      std::cin >> move;
+
+      if (!board.moveTile(move))
+      {
+        std::cout << "Invalid move!" << std::endl;
+      }
+    }
+  std::cout << "Congratulations, you have solved the puzzle" << std::endl;
 }
