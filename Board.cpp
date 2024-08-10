061c755f9b67a727a6100abfb2d3d26da7dcd07c
issak1 <143627558+issak1@users.noreply.github.com>
2024-08-09 20:07:59 -0400

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -17,3 +17,43 @@ Board::Board(int size) : size(size), grid(size, std::vector<Tile>(size)) {
         }
     }
 }
+
+void Board::shuffle() {
+    std::vector<int> values;
+    for (int i = 0; i < size * size; ++i) {
+        values.push_back(i);
+    }
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::shuffle(values.begin(), values.end(), std::default_random_engine(seed));
+
+    int index = 0;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            grid[i][j].setValue(values[index++]);
+        }
+    }
+}
+
+bool Board::moveTile(int tileValue) {
+    int emptyX = -1, emptyY = -1, tileX = -1, tileY = -1;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (grid[i][j].isEmpty()) {
+                emptyX = i;
+                emptyY = j;
+            }
+            if (grid[i][j].getValue() == tileValue) {
+                tileX = i;
+                tileY = j;
+            }
+        }
+    }
+
+    if (isAdjacent(emptyX, emptyY, tileX, tileY)) {
+        swapTiles(emptyX, emptyY, tileX, tileY);
+        return true;
+    }
+
+    return false;
+}
