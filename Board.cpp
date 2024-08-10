471ee9093a2f1f3e1c2e05374bcb4b77f306b6e5
issak1 <143627558+issak1@users.noreply.github.com>
2024-08-09 20:05:42 -0400

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -3,3 +3,17 @@
 #include <algorithm>
 #include <random>
 #include <chrono>
+
+Board::Board(int size) : size(size), grid(size, std::vector<Tile>(size)) {
+    int value = 1;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (i == size - 1 && j == size - 1) {
+                grid[i][j] = Tile(0); // empty tile
+            }
+            else {
+                grid[i][j] = Tile(value++);
+            }
+        }
+    }
+}
