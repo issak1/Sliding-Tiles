7c3946a7217b90b0678ba1bd493c3f2841268563
issak1 <143627558+issak1@users.noreply.github.com>
2024-08-09 20:00:41 -0400

    Update Board.h

--- Board.h
+++ Board.h
@@ -9,4 +9,13 @@ public:
     bool moveTile(int tileValue);
     bool isSolved() const;
     void printBoard() const;
+
+private:
+    int size;
+    std::vector<std::vector<Tile>> grid;
+
+    void swapTiles(int x1, int y1, int x2, int y2);
+    bool isAdjacent(int x1, int y1, int x2, int y2) const;
+};
+
 }
